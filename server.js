c41c1d4afafe87ebf30b6e9ccf1bfa9d28e3fb76
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-22 13:53:09 -0400

    Testing

--- server.js
+++ server.js
@@ -29,16 +29,16 @@ server.options('/*', function(req,res,next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
   res.send(200)
 })
-server.use(function(req,res,next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  if ('OPTIONS' === req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-})
+//server.use(function(req,res,next) {
+  //res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  //res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  //if ('OPTIONS' === req.method) {
+    //res.send(200);
+  //} else {
+    //next();
+  //}
+//})
 server.use(cors())
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json())
